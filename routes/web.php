996628bb3b66aad1f28d084a203ae5f1323efdba
Kate <pancani.caterina@gmail.com>
2024-03-22 00:10:44 +0100

    commento importante

--- routes/web.php
+++ routes/web.php
@@ -5,5 +5,6 @@ use App\Http\Controllers\pippo;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/',[main::class,'home'])->name('home');
+                // {parametro} per rotta parametrica
 Route::get('/ordini/{food}',[main::class,'ordini'])->name('ordini');
 Route::get('piatti',[main::class,'piatti'])->name('piatti');
